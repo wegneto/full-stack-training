f9f950e67fdbc5d5fc05dd03d31a553cda13fd12
Wilson Guimarães <wegneto@gmail.com>
2018-01-31 14:01:26 +0000

    [wip] Lesson 2 - step 16: refactoring

--- 01. Programming Foundations with Python/Lesson 02/break_time.py
+++ 01. Programming Foundations with Python/Lesson 02/break_time.py
@@ -1,3 +1,11 @@
+import time
 import webbrowser
 
-webbrowser.open("http://www.uol.com.br");
+total_breaks = 3;
+break_count = 0;
+
+print("This program started on: " + time.ctime())
+while total_breaks > break_count:
+    time.sleep(10)
+    webbrowser.open("http://www.uol.com.br")
+    break_count = break_count + 1
