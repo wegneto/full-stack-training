40d1f72f754a12c6507728f8317b72493fc1e57b
Wilson Guimarães <wegneto@gmail.com>
2018-02-02 22:34:32 +0000

    21. Opening a file

--- 01. Programming Foundations with Python/Lesson 02/rename_files.py
+++ 01. Programming Foundations with Python/Lesson 02/rename_files.py
@@ -1,6 +1,9 @@
+import os
+
 def rename_files():
-	print("This program started")
 	#1 get files from folder
+	file_list = os.listdir("prank")
+	print(file_list)
 	#2 rename each file
 
 rename_files()
