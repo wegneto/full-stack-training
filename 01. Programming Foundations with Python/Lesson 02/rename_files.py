21677598dd1343457e1b3ecc38b23debc6dae798
Wilson Guimarães <wegneto@gmail.com>
2018-02-03 21:24:29 +0000

    finishing lesson 2

--- 01. Programming Foundations with Python/Lesson 02/rename_files.py
+++ 01. Programming Foundations with Python/Lesson 02/rename_files.py
@@ -3,7 +3,12 @@ import os
 def rename_files():
 	#1 get files from folder
 	file_list = os.listdir("prank")
-	print(file_list)
+	saved_path = os.getcwd()
+	print("Current working directory: " + saved_path)
+	os.chdir("prank")
+	
 	#2 rename each file
+	for file_name in file_list:
+		os.rename(file_name, file_name.translate(None, "0123456789"))
 
 rename_files()
