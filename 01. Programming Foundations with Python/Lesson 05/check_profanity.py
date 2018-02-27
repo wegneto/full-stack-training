f448efdee4ed048d38a90994eb26c8c286c176bb
Wilson Guimarães <wegneto@gmail.com>
2018-02-27 18:50:19 +0000

    [wip] Lesson 5-13: printing a better output

--- 01. Programming Foundations with Python/Lesson 05/check_profanity.py
+++ 01. Programming Foundations with Python/Lesson 05/check_profanity.py
@@ -10,7 +10,13 @@ def read_text():
 def check_profanity(text_to_check):
 	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
 	output = connection.read()
-	print(output)
+	#print(output)
 	connection.close()
+	if "true" in output:
+		print("Profanity Alert!!")
+	elif "false" in output:
+		print("This document has no curse words!")
+	else:
+		print("Could not scan the document properly")
 
 read_text()
