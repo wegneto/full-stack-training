b5ce09f5d059a06b5b88cab1929d684b08444a0c
Wilson Guimarães <wegneto@gmail.com>
2018-03-03 11:29:53 +0000

    [wip] Lesson 6-5: defining __init__

---
+++ 01. Programming Foundations with Python/Lesson 06/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story", 
+						"A story of a boy and his toys that come to life", 
+						"https://pt.wikipedia.org/wiki/Toy_Story#/media/File:Movie_poster_toy_story.jpg",
+						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
