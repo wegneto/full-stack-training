375c1a2034d6417f97c6842db7b083a2423991fe
Wilson Guimarães <wegneto@gmail.com>
2018-03-03 12:19:47 +0000

    Lesson 6: movie website

--- 01. Programming Foundations with Python/Lesson 06/fresh_tomatoes.py
+++ 01. Programming Foundations with Python/Lesson 06/fresh_tomatoes.py
@@ -9,7 +9,7 @@ main_page_head = '''
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>Movie Catalog</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
