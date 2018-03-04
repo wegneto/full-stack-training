6143a78d2916eefdbed79c4f5f2ac1830394512c
Wilson Guimarães <wegneto@gmail.com>
2018-03-04 17:35:19 +0000

    Lesson 7-6

--- 01. Programming Foundations with Python/Lesson 07/inheritance.py
+++ 01. Programming Foundations with Python/Lesson 07/inheritance.py
@@ -4,5 +4,15 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
+class Child(Parent):
+		def __init__(self, last_name, eye_color, number_of_toys):
+			print("Child constructor called")
+			Parent.__init__(self, last_name, eye_color)
+			self.number_of_toys = number_of_toys
+
 john_doe = Parent("Doe", "brown")
 print(john_doe.last_name)
+
+jane_doe = Child("Doe", "brown", 5)
+print(jane_doe.last_name)
+print(jane_doe.number_of_toys)
